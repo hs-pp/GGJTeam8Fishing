06d2566022c2239c4613ad7a1719b596bfcae3a6
Wanderer <32968283+Brotega@users.noreply.github.com>
2024-01-27 16:25:17 -0800

    Autoreel

--- Assets/Fish/CatchPoint.cs
+++ Assets/Fish/CatchPoint.cs
@@ -6,19 +6,18 @@ public class CatchPoint : MonoBehaviour
 {
     [SerializeField]
     private FishRender m_fishRender;
-    
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    public void CatchFish(Transform hookPoint)
     {
-        // temp? logic to catch fish
-        HookController hookController = collision.gameObject.GetComponent<HookController>();
-        if(hookController != null)
-        {
-            Transform hookPoint = hookController.GetHookPoint().transform;
-            m_fishRender.FishInstance.transform.SetParent(hookPoint);
-            m_fishRender.FishInstance.SetState(FishState.Caught);
-            m_fishRender.FishInstance.Rotate(new Vector3(0, 0, -90));
-            Vector3 offset = transform.rotation * transform.localPosition;
-            m_fishRender.FishInstance.transform.position = hookPoint.position - offset;
-        }
-    }
+		m_fishRender.FishInstance.transform.SetParent(hookPoint);
+		m_fishRender.FishInstance.SetState(FishState.Caught);
+		m_fishRender.FishInstance.Rotate(new Vector3(0, 0, -90));
+		Vector3 offset = transform.rotation * transform.localPosition;
+		m_fishRender.FishInstance.transform.position = hookPoint.position - offset;
+	}
+
+	public FishInstance GetFishInstance()
+	{
+		return m_fishRender.FishInstance;
+	}
 }
--- Assets/Teddy Tunic/HookController.cs
+++ Assets/Teddy Tunic/HookController.cs
@@ -4,52 +4,108 @@ using UnityEngine;
 
 public class HookController : MonoBehaviour
 {
-	[SerializeField] LineRender Line; // Fishing rod line, don't confuse the name with LineRender(er)
-	[SerializeField] GameObject HookPoint; // The point where the fish get snapped to
-	[SerializeField] float Velocity;
-	[SerializeField] float MaxDistance;
+	[SerializeField] LineRender line; // Fishing rod line, don't confuse the name with LineRender(er)
+	[SerializeField] GameObject hookPoint; // The point where the fish get snapped to
+	[SerializeField] float velocity;
+	[SerializeField] float maxDistance;
+	[SerializeField] float autoreelVelocity;
+	[SerializeField] float dropoffDistance; // Make this a tiny number
 
-	Vector3 _moveDirection;
-	
-	public Transform GetHookPoint()
+	public enum State
 	{
-		return HookPoint.transform;
+		PLAYER_CONTROLLED,
+		AUTOREELING
+	}
+
+	Vector3 _startPosition;
+	Vector3 _moveDirection;
+	State _currentState;
+	FishInstance _caughtFish;
+
+	public void SetState(State state) => _currentState = state;
+
+	private void Start()
+	{
+		_startPosition = transform.position;
+		_currentState = State.PLAYER_CONTROLLED;
 	}
 
 	private void Update()
 	{
-		_moveDirection = Vector2.zero;
+		switch(_currentState)
+		{
+			case State.PLAYER_CONTROLLED:
 
-		if (Input.GetKey(KeyCode.W))
-		{
-			_moveDirection += Vector3.up;
-		}
-		if (Input.GetKey(KeyCode.A))
-		{
-			_moveDirection += Vector3.left;
-		}
-		if (Input.GetKey(KeyCode.S))
-		{
-			_moveDirection += Vector3.down;
-		}
-		if (Input.GetKey(KeyCode.D))
-		{
-			_moveDirection += Vector3.right;
-		}
+				_moveDirection = Vector2.zero;
 
-		_moveDirection.Normalize();
+				if (Input.GetKey(KeyCode.W))
+				{
+					_moveDirection += Vector3.up;
+				}
+				if (Input.GetKey(KeyCode.A))
+				{
+					_moveDirection += Vector3.left;
+				}
+				if (Input.GetKey(KeyCode.S))
+				{
+					_moveDirection += Vector3.down;
+				}
+				if (Input.GetKey(KeyCode.D))
+				{
+					_moveDirection += Vector3.right;
+				}
+
+				_moveDirection.Normalize();
+
+				break;
+
+			case State.AUTOREELING:
+
+				Vector3 hookToStart = _startPosition - transform.position;
+
+				// Fish is caught
+				if (_startPosition == transform.position || hookToStart.sqrMagnitude <= (dropoffDistance * dropoffDistance))
+				{
+					_currentState = State.PLAYER_CONTROLLED;
+
+					// TODO: Let the game manager know we caught the fish
+					// Dunno if we should destroy but leaving it for now
+					Destroy(_caughtFish.gameObject);
+					_caughtFish = null;
+				}
+				else
+				{
+					_moveDirection = hookToStart.normalized;
+
+					Vector3 displacement = _moveDirection * autoreelVelocity * Time.deltaTime;
+					transform.position += displacement;
+				}
+
+				break;
+		}
 	}
 
 	private void FixedUpdate()
 	{
-		Vector3 displacement =  _moveDirection * Velocity * Time.fixedDeltaTime;
+		Vector3 displacement =  _moveDirection * velocity * Time.fixedDeltaTime;
 		Vector3 newPosition = transform.position + displacement;
-		Vector3 hookToLine = Line.transform.position - newPosition;
+		Vector3 hookToLine = line.transform.position - newPosition;
 
 		// Update hook's position as long as it's new position is within max distance
-		if (hookToLine.sqrMagnitude <= (MaxDistance * MaxDistance))
+		if (hookToLine.sqrMagnitude <= (maxDistance * maxDistance))
 		{
 			transform.position = newPosition;
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		CatchPoint catchPoint = collision.gameObject.GetComponent<CatchPoint>();
+		if (catchPoint != null)
+		{
+			catchPoint.CatchFish(hookPoint.transform);
+			_caughtFish = catchPoint.GetFishInstance();
+			_currentState = State.AUTOREELING;
 		}
 	}
 }
