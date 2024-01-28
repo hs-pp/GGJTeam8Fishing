17419521219111e567dea85a94b98a6366517a0a
Wanderer <32968283+Brotega@users.noreply.github.com>
2024-01-28 11:28:39 -0800

    Full Flow with Docks Dialogue + Newpaper Scenes

--- Assets/Docks/DocksDialogueStarter.cs
+++ Assets/Docks/DocksDialogueStarter.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class DocksDialogueStarter : MonoBehaviour
 {
     [SerializeField] DialogueScene dialogueScene;
-    [SerializeField] DocksDialogueScriptableObject docksDialogueLibrary;
+    [SerializeField] DayLibrary dayLibrary;
 	[SerializeField] SceneLoader sceneLoader;
 
 	private void Start()
 	{
-		dialogueScene.NewScene(docksDialogueLibrary.AllDocksDialogue[GameStateManager.GetDay() - 1].Dialogue);
+		dialogueScene.NewScene(dayLibrary.Days[GameStateManager.GetDay() - 1].DocksDialogue);
 		dialogueScene.OnDialogueFinish += sceneLoader.LoadFishingScene;
 	}
 }
