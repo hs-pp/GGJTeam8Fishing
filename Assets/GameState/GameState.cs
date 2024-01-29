d7556c2f604ea106d9e5d1aa14614b0a4f584bea
Hyungjin <hshin2017@gmail.com>
2024-01-28 16:29:32 -0800

    gamestate oop

--- Assets/GameState/GameState.cs
+++ Assets/GameState/GameState.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 [Serializable]
 public class GameState
 {
-    public int Day = 9;
+    public int Day = 1;
     public int Money = 0;
     public int BaitAmount = 5;
     public List<string> CaughtFishUniqueIds = new();
